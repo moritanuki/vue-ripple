9e5c2c0daea8634409f53269afa055ac2a07af32
moritanuki <ilovfam12@gmail.com>
2024-06-15 14:40:52 +0900

    Vue Routerの導入

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,3 @@
-<script setup lang="ts">
-import Ripple from '@/components/Ripple.vue'
-</script>
 <template>
-  <div class="w-screen h-screen flex justify-center items-center">
-    <button class="bg-gray h-16 p-4 rounded overflow-hidden relative">tailwind
-      <Ripple />
-    </button>
-  </div>
+  <router-view />
 </template>

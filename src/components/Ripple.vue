bde844b8f26ac373800f170940b005ddb7c21de2
moritanuki <ilovfam12@gmail.com>
2024-06-16 21:25:02 +0900

    rippleイベントの高速化
    * clickイベントではなくmousedownとtouchstartに分割

--- src/components/Ripple.vue
+++ src/components/Ripple.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, onBeforeUnmount, ref, Ref } from 'vue'
+
+type EventType = 'mouse' | 'touch' | undefined
+
+const stopSymbol = Symbol('rippleStop')
+type RippleEvent = (MouseEvent | TouchEvent) & {
+  [stopSymbol]?: boolean
+}
 
 // 波紋の広がり速度
 const DURATION_MS = 300
 
-const ripple = ref()
-const radius = ref(0)
+const ripple: Ref<HTMLElement | undefined> = ref()
+const eventType: Ref<EventType> = ref()
 
 const sleep = (ms: number): Promise<void> => {
   return new Promise((resolve) => {
@@ -13,35 +20,89 @@ const sleep = (ms: number): Promise<void> => {
   })
 }
 
-const rippleAnimation = async (event: MouseEvent) => {
+const isTouchEvent = (e: Event): e is TouchEvent => {
+  return e.constructor.name === 'TouchEvent'
+}
+
+const rippleShow = async (e: RippleEvent) => {
+  // preventDefaultと同じ役割
+  if (e[stopSymbol]) return;
+  e[stopSymbol] = true;
 
   const animation = document.createElement('span')
+  if (!ripple.value) return
   ripple.value.appendChild(animation)
 
   // 円の半径（下記なら親要素より2倍幅の円になる）
-  radius.value = ripple.value.clientWidth
+  const radius = ripple.value.clientWidth
 
-  // NOTE: 'pointer-events-none'を付与しないと波紋要素のeventを拾ってしまう
+  // NOTE: 'pointer-es-none'を付与しないと波紋要素のeを拾ってしまう
   animation.classList.add('absolute', 'rounded-full', 'scale-0', 'bg-black', 'opacity-20', 'pointer-events-none')
-  animation.style.width = `${radius.value * 2}px`
-  animation.style.height = `${radius.value * 2}px`
+  animation.style.width = `${radius * 2}px`
+  animation.style.height = `${radius * 2}px`
+
+  let offsetX: number
+  let offsetY: number
+
+  if (isTouchEvent(e)) {
+    const touch = e.touches[e.touches.length - 1]
+    const boundingRect = ripple.value.getBoundingClientRect()
+    offsetY = touch.clientY - boundingRect.top
+    offsetX = touch.clientX - boundingRect.left
+
+  } else {
+    offsetY = e.offsetY
+    offsetX = e.offsetX
+  }
 
   // offsetの値をそのまま渡すとクリックした場所が円の左上になる
   // そのため、半径の値を引くことでクリックした場所が円の中心になるようにしている
-  animation.style.top = `${event.offsetY - radius.value}px`
-  animation.style.left = `${event.offsetX - radius.value}px`
+  animation.style.top = `${offsetY - radius}px`
+  animation.style.left = `${offsetX - radius}px`
 
   await sleep(1)
   animation.style.transitionDuration = `${DURATION_MS}ms`
   animation.classList.add('scale-100')
 
   await sleep(DURATION_MS)
+  if (!ripple.value) return; // 先に遷移が走ってremoveできない
   ripple.value.removeChild(animation)
 }
+
+const rippleAnimation = (type: EventType, e: RippleEvent) => {
+  // 初めに検知したイベントだけを走らせることによってイベントの波及を防ぐ
+  // preventDefaultと同じ役割（preventDefaultを使うと親要素のイベントも止めてしまうため）
+  if (!eventType.value) eventType.value = type
+  if (eventType.value === type) {
+    rippleShow(e)
+  }
+}
+
+onMounted(() => {
+  if (!ripple.value) return
+
+  ripple.value.addEventListener('mousedown', (e: MouseEvent) => {
+    rippleAnimation('mouse', e)
+  })
+  ripple.value.addEventListener('touchstart', (e: TouchEvent) => {
+    rippleAnimation('touch', e)
+  })
+})
+
+onBeforeUnmount(() => {
+  if (!ripple.value) return
+
+  ripple.value.removeEventListener('mousedown', (e: MouseEvent) => {
+    rippleAnimation('mouse', e)
+  })
+  ripple.value.removeEventListener('touchstart', (e: TouchEvent) => {
+    rippleAnimation('touch', e)
+  })
+})
 </script>
 <template>
   <!-- inset は CSS のプロパティで、 top, right, bottom, left に対応する一括指定です。これは margin の一括指定における複数値の場合と同じ構文です。 -->
   <!-- 引用元: https://developer.mozilla.org/ja/docs/Web/CSS/inset -->
   <!-- NOTE: insetを使うことで親要素と同じサイズに指定している -->
-  <span ref="ripple" class="absolute inset-0" @click="rippleAnimation"></span>
+  <span ref="ripple" class="absolute inset-0"></span>
 </template>
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
 import Ripple from '@/components/Ripple.vue'
+
+const router = useRouter()
+
+const sleep = (ms: number): Promise<void> => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+const toAbout = async () => {
+  await sleep(300)
+  router.push({ name: 'About' })
+}
 </script>
 <template>
   <div class="w-screen h-screen flex justify-center items-center">
-    <button class="bg-gray h-16 p-4 rounded overflow-hidden relative">tailwind
+    <button class="bg-gray h-16 p-4 rounded overflow-hidden relative" @click="toAbout">tailwind
       <Ripple />
     </button>
   </div>
